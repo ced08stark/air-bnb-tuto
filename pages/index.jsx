b246b4fd2831aa9d65842df46af48e7abbcc5b22
ced_stark08 <cedricngangue08@gmail.com>
2022-10-17 11:46:44 +0200

    change case of file-route

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ import SmallCard from "../components/SmallCard"
 import MeduimCard from "../components/MediumCard"
 import LargeCard from "../components/LargeCard"
 import Footer from "../components/Footer"
-import Header from "../Components/Header.jsx";
+import Header from "../components/Header.jsx";
 import Banner from "../components/Banner";
 
 const Home = () => {
